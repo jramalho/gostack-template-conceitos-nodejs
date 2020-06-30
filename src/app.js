3fb916182e77bdf74cf272092b0bccb29c306fc8
Ramalho <jonathan.oliveira.ramalho@gmail.com>
2020-06-29 21:38:26 -0300

    mini-api like DONE

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,65 @@ app.use(cors());
 
 const repositories = [];
 
+function fetchRepository(id, response){
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+  if(repoIndex === -1){
+    return response.status(400).send({Error: 'Repository Not Found'});
+  }
+  return repoIndex
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).send(repositories)
 });
 
+
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  let repo = {
+    id:uuid(),
+    title,
+    url,
+    techs,
+    likes:0,
+  }
+
+  repositories.push(repo);
+
+  return response.status(200).send(repo)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const {techs, title, url} = request.body;
+  const repoToUpdate = fetchRepository(id, response);
+
+  const repository =  {...repositories[repoToUpdate], ...{techs, title, url}};
+  repositories[repoToUpdate] = repoToUpdate;
+
+  return response.status(200).send(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoToDelete = fetchRepository(id, response);
+
+  repositories.splice(repoToDelete, 1);
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoToLike = fetchRepository(id, response);
+
+  repositories[repoToLike].likes++;
+
+  return response.status(200).send({likes: repositories[repoToLike].likes})
 });
 
 module.exports = app;
